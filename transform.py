a69b9bfc71641173bfcf87a118d109d50b78a604
Honorisverum <cheekibreeki322@gmail.com>
2019-03-28 19:07:47 +0300

    add gitignore

--- transform.py
+++ transform.py
@@ -149,6 +149,7 @@ class MakeLSTM(nn.Module):
 
         now_frame = x[0].unsqueeze(0).unsqueeze(0)
         #inpt = torch.cat([x[:-1], y], dim=1)
+        x[1:].detach_()
 
         """
         =============================
